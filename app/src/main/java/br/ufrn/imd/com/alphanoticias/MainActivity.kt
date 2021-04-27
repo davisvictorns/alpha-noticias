a79c61fe6096afa2f4eee96e7507cb1bef424c0f
davisvictorns <davis_victor@hotmail.com>
2021-04-27 20:35:22 -0300

    First commit

---
+++ app/src/main/java/br/ufrn/imd/com/alphanoticias/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.ufrn.imd.com.alphanoticias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
