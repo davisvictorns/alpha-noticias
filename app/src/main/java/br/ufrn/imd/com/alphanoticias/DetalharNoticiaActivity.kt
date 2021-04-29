326e02d52d50539f38da38e02ca1d07f19cf0e17
Gabriel Vitor da Silva Medeiros <gabrielvsmedeiros@gmail.com>
2021-04-29 20:18:25 -0300

    adicao de tela de listagem de noticia - sem listar a noticia ainda

---
+++ app/src/main/java/br/ufrn/imd/com/alphanoticias/DetalharNoticiaActivity.kt
@@ -0,0 +1,11 @@
+package br.ufrn.imd.com.alphanoticias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalharNoticiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhar_noticia)
+    }
+}
