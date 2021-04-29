9edb426fb252f00329e2d8968760f6dd0fa227f0
Gabriel Vitor da Silva Medeiros <gabrielvsmedeiros@gmail.com>
2021-04-29 20:02:29 -0300

    adicao de tela de configuracoes

--- app/src/main/java/br/ufrn/imd/com/alphanoticias/Convite.kt
+++ app/src/main/java/br/ufrn/imd/com/alphanoticias/Convite.kt
@@ -2,7 +2,13 @@ package br.ufrn.imd.com.alphanoticias
 
 import java.time.LocalDateTime
 
-class Convite(val email: String, val tipo: String, val aceito: Boolean? = false, val enviado_por_id: String? = null, val enviado_as: LocalDateTime? = LocalDateTime.now()) {
+class Convite(
+    val email: String,
+    val tipo: String,
+    val aceito: Boolean? = false,
+    val enviado_por_id: String? = null,
+    val enviado_as: LocalDateTime? = LocalDateTime.now()
+) {
     constructor() : this("", "", false, "", LocalDateTime.now()) {
 
     }
